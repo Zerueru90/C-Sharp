004f9ddbc7af8ef9406d47b7aa57722bd04a4748
Samson <31880780+Zerueru90@users.noreply.github.com>
2020-09-15 14:09:21 +0200

    klar

--- Uppgift2/Villkor_och_loopar/Program.cs
+++ Uppgift2/Villkor_och_loopar/Program.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace Villkor_och_loopar
 {
-
     /// <summary>
     
        
@@ -12,9 +18,244 @@ namespace Villkor_och_loopar
     {
         static void Main(string[] args)
         {
-            var x = Console.ReadLine();
+            int startnummer, starttimme, startminut, startsekund,
+                             måltimme, målminut, målsekund;
+            int bästastartnummer = 0, deltagare = 0, nästBästaStartNummer = 0,  sluttid, 
+                nästaBästaTid = int.MaxValue, bästaTid = int.MaxValue;
 
-            
+            bool loop = true;
+            bool gameloop = true;
+            while (gameloop)
+            {
+
+                while (loop)
+                {
+                    Console.Write("\nAnge startnummer: ");
+                    bool korrektInput = int.TryParse(Console.ReadLine(), out startnummer);
+
+                    if (startnummer < 1 && deltagare == 0)
+                    {
+
+                        Console.WriteLine("Inga tävlande");
+                        loop = false;
+                        gameloop = false;
+                        Console.ReadLine();
+                        break;
+                    }
+                    else if (startnummer < 1)
+                    {
+                        loop = false;
+                        break;
+                    }
+
+                    starttimme = ReturnUserInput("\nAnge startimme: ", 0, 23);
+                    startminut = ReturnUserInput("Ange startminut: ", 0, 59);
+                    startsekund = ReturnUserInput("Ange startsekund: ", 0, 59);
+                    måltimme = ReturnUserInput("\nAnge måltimme: ", 0, 23);
+                    målminut = ReturnUserInput("Ange målminut: ", 0, 59);
+                    målsekund = ReturnUserInput("Ange målsekund: ", 0, 59);
+
+                    int totalSekunderStart = ConvertToSeconds(starttimme, startminut, startsekund);
+
+                    int totalSekunderMål = ConvertToSeconds(måltimme, målminut, målsekund);
+
+                    if (starttimme == 0 && måltimme == 0 && startminut == 0 && målminut == 0)
+                    {
+
+                        sluttid = SecondsFix(startsekund, målsekund);
+
+                    }
+                    else if (totalSekunderStart > totalSekunderMål)
+                    {
+                        sluttid = MidnightFix(starttimme, startminut, startsekund, måltimme, målminut, målsekund);
+                    }
+                    else
+                    {
+                        sluttid = totalSekunderMål - totalSekunderStart;
+                    }
+
+                    if (sluttid < bästaTid)
+                    {
+                        bästaTid = sluttid;
+                        bästastartnummer = startnummer;
+                    }
+                    else if (sluttid < nästaBästaTid)
+                    {
+                        nästaBästaTid = sluttid;
+                        nästBästaStartNummer = startnummer;
+                    }
+                    deltagare++;
+
+                }
+                if (deltagare != 0)
+                {
+                    //First place
+                    string textFirstPlaceValue = ConvertBackFromSeconds(bästaTid);
+
+                    string[] words = textFirstPlaceValue.Split('/');
+                    int hour = 0, min = 0, sec = 0;
+
+                    for (int i = 0; i < words.Length;)
+                    {
+                        hour = Convert.ToInt32(words[0]);
+                        min = Convert.ToInt32(words[1]);
+                        sec = Convert.ToInt32(words[2]);
+                        break;
+                    }
+
+                    Console.WriteLine($"\nFörstaplats: startnummer {bästastartnummer}");
+                    Console.WriteLine($"Förstaplats måltid: {hour} timme, {min} min, {sec} sek");
+                    Console.WriteLine($"Deltagare: {deltagare}");
+
+                    //Second place
+                    if (deltagare > 1)
+                    {
+                        string textSecondPlaceValue = ConvertBackFromSeconds(nästaBästaTid);
+
+                        string[] words2 = textSecondPlaceValue.Split('/');
+                        int hour2 = 0, min2 = 0, sec2 = 0;
+
+                        for (int i = 0; i < words2.Length;)
+                        {
+                            hour2 = Convert.ToInt32(words2[0]);
+                            min2 = Convert.ToInt32(words2[1]);
+                            sec2 = Convert.ToInt32(words2[2]);
+                            break;
+                        }
+
+                        Console.WriteLine($"\nAndraplats: startnummer {nästBästaStartNummer}");
+                        Console.WriteLine($"Andraplats måltid: {hour2} timme, {min2} min, {sec2} sek");
+                        Console.WriteLine($"Deltagare: {deltagare}");
+                    }
+
+                    Console.WriteLine("Starta om? y/n");
+                    string answer = Console.ReadLine();
+
+                    if (answer == "n")
+                    {
+                        gameloop = false;
+                    }
+                    else if (answer == "y")
+                    {
+                        loop = true;
+                        deltagare = 0;
+                    }
+                }
+            }
         }
+
+        public static int SecondsFix(int nightSec, int noonSec)
+        {
+            int sum = nightSec - noonSec;
+
+            int h = 23, m = 59, s = 60;
+
+            int summy = s - sum;
+
+            int summy2 = ConvertToSeconds(h, m, summy);
+
+            return summy2;
+        }
+
+        public static int MidnightFix(int nightHour, int nightMin, int nightSec, int noonHour, int noonMin, int noonSec)
+        {
+            int midnight = 24;
+            int passedmidnight = 0;
+
+            int minN = 60;
+
+            if (nightHour == 23 || nightHour == 0)
+            {
+                midnight = 23;
+            }
+
+            int hoursLeftToMidnight = midnight - nightHour;
+
+            if (nightMin == 0)
+            {
+                minN = 0;
+            }
+            int midnightMin = minN - nightMin;
+            int midnightSec = nightSec;
+
+
+            int hoursPastMidnight = noonHour - passedmidnight;
+            int passedPastMidnightMin = noonMin;
+            int passedPastMidnightSec = noonSec;
+
+
+            int totalHours = hoursLeftToMidnight + hoursPastMidnight;
+            int totalMin = midnightMin + passedPastMidnightMin;
+            int totalSec = midnightSec + passedPastMidnightSec;
+
+            int totalConvertedSec = ConvertToSeconds(totalHours, totalMin, totalSec);
+
+            return totalConvertedSec;
+        }
+
+        public static int ConvertToSeconds(int hour, int min, int sec)
+        {
+            int timmeISekund = hour * 3600;
+
+            int minutISekund = min * 60;
+
+            int totalSekunderStart = timmeISekund + minutISekund + sec;
+
+            return totalSekunderStart;
+        }
+
+        public static string ConvertBackFromSeconds(int winnerHighestValue)
+        {
+            double hour = winnerHighestValue / 3600;
+
+            int finishHour = (int)hour;
+
+            double minDouble = finishHour * 3600;
+
+            double minDouble1 = winnerHighestValue - minDouble;
+
+            double minDouble2 = minDouble1 / 60;
+
+            int finishMin = (int)minDouble2;
+
+            double secDouble = finishMin * 60;
+
+            double sec = minDouble1 - secDouble;
+
+            int finishSec = (int)sec;
+
+            string test = Convert.ToString($"{finishHour} / {finishMin} / {finishSec}");
+
+            return test;
+        }
+
+        public static bool CheckCorrectInputHour(int number)
+        {
+            return number > 23 || number < 0;
+        }
+
+        public static bool CheckCorrectInputMinSec(int number)
+        {
+            return number > 59 || number < 0;
+        }
+
+        public static int ReturnUserInput(string msg, int minValue, int maxValue)
+        {
+            bool convertOK = false;
+            int timeUnit;
+            string input;
+
+            do
+            {
+                Console.Write($"{msg} ");
+                input = Console.ReadLine();
+                convertOK = int.TryParse(input, out timeUnit);
+                
+                if (convertOK)
+                    convertOK = timeUnit <= maxValue && timeUnit >= minValue;
+            } while (!convertOK);
+            return timeUnit;
+        }
+
     }
 }
--- Uppgift3/Klasser/Program.cs
+++ Uppgift3/Klasser/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace Klasser
 {
@@ -10,14 +12,131 @@ namespace Klasser
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            string regnr;
+            string modellnamn;
+            DateTime regdatum;
+            int vikt;
+            string namn;
+            int ålder;
+            bool elbil = false;
 
-            Console.WriteLine("Hello World!");
+            Bil bil;
+            Person person;
+            List <Person> listPersoner = new List<Person>();
+            bool loop = true;
+
+            while (loop)
+            {
+                Console.Write("\nModel: ");
+                modellnamn = Console.ReadLine();
+
+                Console.Write("Reg nr: ");
+                regnr = Console.ReadLine();
+
+                regdatum = DateTime.Today;
+                Console.Write($"Reg datum: {regdatum}");
+
+                Console.Write("\nVikt: ");
+                vikt = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Är detta en elbil, y/n: ");
+                string answer = Console.ReadLine();
+                if (answer == "y")
+                {
+                    elbil = true;
+                }
+                else if (answer == "n")
+                {
+                    elbil = false;
+                }
+                else
+                {
+                    //Om någon inte skriver in yes eller no.
+                }
+
+                bil = new Bil(modellnamn, vikt, regdatum, elbil, regnr);
+
+                Console.Write("Namn: ");
+                namn = Console.ReadLine();
+
+                Console.Write("Ålder: ");
+                ålder = int.Parse(Console.ReadLine());
+
+                person = new Person(namn, ålder, bil);
+
+                listPersoner.Add(person);
+
+                foreach (var item in listPersoner)
+                {
+                    Console.WriteLine($"\nNamn: {item.Namn} " +
+                                      $"\nÅlder: {item.Ålder} " +
+                                      $"\nModel: {item.Bil.Modellnamn} " +
+                                      $"\nRegistreringsnummer: {item.Bil.RegNr} " +
+                                      $"\nReg Datum: {item.Bil.DateTime} " +
+                                      $"\n {item.Bil.ToString()}");
+                }
+
+                Console.Write("\nDo you want to quit? y/n: ");
+                string quit = Console.ReadLine();
+
+                if (quit == "y")
+                {
+                    loop = false;
+                }
+            }
         }
     }
 
     public class Bil
     {
-        public string Registreringsnummer { get; set; }
+        //Properties
+        public Int32 Vikt { get; set; }
+        public DateTime DateTime { get; set; }
+        public Boolean Elbil { get; set; }
 
+        //Fält
+        public string Modellnamn;
+        protected decimal Milmätare;
+        public string RegNr;
+
+        public Bil(string modellnamn, int vikt, DateTime dateTime, bool elbil, string regnr)
+        {
+            Vikt = vikt;
+            DateTime = dateTime;
+            Elbil = elbil;
+            Modellnamn = modellnamn;
+            RegNr = regnr;
+        }
+
+        public override string ToString()
+        {
+            if (Elbil == true)
+            {
+                return $" Detta är en elbil! ";
+            }
+            else
+                return $" Detta är INTE en elbil! ";
+        }
+
+        public string UppdateraMS(decimal ms)
+        {
+            Milmätare += ms;
+            string stringMS = Convert.ToString(Milmätare);
+            return stringMS;
+        }
+    }
+
+    public class Person
+    {
+        public String Namn { get; set; }
+        public Int32 Ålder { get; set; }
+        public Bil Bil { get; set; }
+        
+        public Person(string namn, int ålder, Bil bil)
+        {
+            Namn = namn;
+            Ålder = ålder;
+            Bil = bil;
+        }
     }
 }
