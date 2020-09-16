594759006ae545e85a10a507b08429638a852f9b
Samson <31880780+Zerueru90@users.noreply.github.com>
2020-09-16 18:14:46 +0200

    Uppgift4, grunden klar.

--- Uppgift4/ArvOchAbstraktion/Program.cs
+++ Uppgift4/ArvOchAbstraktion/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ArvOchAbstraktion
 {
-    class Program
+    class Program 
     {
+        /*
+            När du fått det att fungera kan du prova att skapa en ny klass, VerkstadV2, 
+            ärva av IVerkstad men skriva annan funktionalitet. 
+            Se att användning av IVerkstad i Main inte påverkas. (Styrkan med ett interface).
+        */
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Verkstad vst = new Verkstad();
+            vst.läggtillFordon();
+
+        }
+
+    }
+    interface IVerkstad
+    {
+        void läggtillFordon();
+        void tabortFordon();
+    }
+
+    class Verkstad : IVerkstad
+    {
+        public void läggtillFordon()
+        {
+
+        }
+        public void tabortFordon()
+        {
+
+        }
+    }
+
+    class VerkstadV2 : IVerkstad
+    {
+        public void läggtillFordon()
+        {
+
+        }
+        public void tabortFordon()
+        {
+
         }
     }
 }
--- Uppgift4/Klasser/Class1.cs
+++ Uppgift4/Klasser/Class1.cs
@@ -2,7 +2,34 @@
 
 namespace Klasser
 {
-    public class Class1
+    public class Bil 
     {
+        public string Modellnamn { get; set; }
+        public string Registreringsnummer { get; set; }
+        public string Mätare { get; set; }
+        public string Registreringsdatum { get; set; }
+
+        private bool Dragkrok { get; set; }
+
+
+    }
+    public class Motorcykel : Bil
+    {
+        public int Maxfart { get; set; }
+
+        
+
+    }
+    public class Buss : Bil
+    {
+        public int Passagerare { get; set; }
+
+        
+    }
+    public class Lastbil : Bil
+    {
+        public int Maxlast { get; set; }
+
+
     }
 }
